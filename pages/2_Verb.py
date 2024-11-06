9c5dbc100b680ea9df6f1f36afb82016319e830e
torbjoau <torbjoau@stud.ntnu.no>
2024-11-06 20:53:42 +0100

    Lagt til uregelmessige verb [card3]

--- pages/2_Verb.py
+++ pages/2_Verb.py
@@ -17,6 +17,8 @@ st.write("# Verb")
 verbs_df = pd.read_csv('data/verbs.csv', sep=',')
 verbs_df.index += 1
 
+irrVerbs_df = pd.read_csv('data/irregular_verbs.csv', sep=';')
+
 # The most common 100 verbs
 with st.expander("# De mest brukte 100 verbene"):
   col1, col2, col3 = st.columns(3)
@@ -30,7 +32,14 @@ with st.expander("# De mest brukte 100 verbene"):
 
 # Irregular verbs
 with st.expander("# Uregelmessige verb"):
-  st.write("Sah dude")
+  col1, col2, col3 = st.columns(3)
+  first_third, second_third = irrVerbs_df.shape[0] // 3, 2 * irrVerbs_df.shape[0] // 3
+  with col1:
+    st.dataframe(irrVerbs_df.iloc[:first_third], use_container_width=True, height=35*first_third+38)
+  with col2:
+    st.dataframe(irrVerbs_df.iloc[first_third:second_third], use_container_width=True, height=35*first_third+38)
+  with col3:
+    st.dataframe(irrVerbs_df.iloc[second_third:], use_container_width=True, height=35*first_third+38)
 
 # st.data_editor(
 #     verbs_df,
