d8b5fde08b646afec1d29d286e3169a8134bc893
Haakon Flaaronning <haakon@flaaronning.net>
2024-11-06 22:40:26 +0100

    Fix some shit

--- pages/2_Verb.py
+++ pages/2_Verb.py
@@ -13,7 +13,9 @@ verbs_df = pd.read_csv('data/verbs.csv', sep=',')
 verbs_df.index += 1
 
 irr_verbs_df = pd.read_csv('data/irregular_verbs.csv', sep=';')
+irr_verbs_df.index += 1
 reflexive_verbs_df = pd.read_csv('data/reflexive_verbs.csv', sep=';')
+reflexive_verbs_df.index += 1
 
 # The most common 100 verbs
 with st.expander("# De mest brukte 100 verbene"):
@@ -28,15 +30,13 @@ with st.expander("# De mest brukte 100 verbene"):
 
 # Irregular verbs
 with st.expander("# Uregelmessige verb"):
-  col1, col2, col3 = st.columns(3)
-  first_third, second_third = irr_verbs_df.shape[0] // 3, 2 * irr_verbs_df.shape[0] // 3
+  col1, col2 = st.columns(2)
+  half_len = irr_verbs_df.shape[0] // 2
   with col1:
-    st.dataframe(irr_verbs_df.iloc[:first_third], use_container_width=True, height=35*first_third+38)
+    st.dataframe(irr_verbs_df.iloc[:half_len], use_container_width=True, height=35*half_len+38)
   with col2:
-    st.dataframe(irr_verbs_df.iloc[first_third:second_third], use_container_width=True, height=35*first_third+38)
-  with col3:
-    st.dataframe(irr_verbs_df.iloc[second_third:], use_container_width=True, height=35*first_third+38)
+    st.dataframe(irr_verbs_df.iloc[half_len:], use_container_width=True, height=35*half_len+38)
 
 # Reflexive verbs
 with st.expander("Refleksive verb"):
-  st.dataframe(reflexive_verbs_df, use_container_width=True, height=35*len(reflexive_verbs_df)+38, hide_index=True)
+  st.dataframe(reflexive_verbs_df, use_container_width=True, height=35*len(reflexive_verbs_df)+38)
