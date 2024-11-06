dab4cba146bf9fe93cb3a977a943c652908cf1bf
torbjoau <torbjorn_aune97@hotmail.com>
2024-11-06 21:09:02 +0100

    hotfix [card3]

--- pages/2_Verb.py
+++ pages/2_Verb.py
@@ -17,7 +17,7 @@ st.write("# Verb")
 verbs_df = pd.read_csv('data/verbs.csv', sep=',')
 verbs_df.index += 1
 
-irrVerbs_df = pd.read_csv('data/irregular_verbs.csv', sep=';')
+irr_verbs_df = pd.read_csv('data/irregular_verbs.csv', sep=';')
 
 # The most common 100 verbs
 with st.expander("# De mest brukte 100 verbene"):
@@ -33,21 +33,10 @@ with st.expander("# De mest brukte 100 verbene"):
 # Irregular verbs
 with st.expander("# Uregelmessige verb"):
   col1, col2, col3 = st.columns(3)
-  first_third, second_third = irrVerbs_df.shape[0] // 3, 2 * irrVerbs_df.shape[0] // 3
+  first_third, second_third = irr_verbs_df.shape[0] // 3, 2 * irr_verbs_df.shape[0] // 3
   with col1:
-    st.dataframe(irrVerbs_df.iloc[:first_third], use_container_width=True, height=35*first_third+38)
+    st.dataframe(irr_verbs_df.iloc[:first_third], use_container_width=True, height=35*first_third+38)
   with col2:
-    st.dataframe(irrVerbs_df.iloc[first_third:second_third], use_container_width=True, height=35*first_third+38)
+    st.dataframe(irr_verbs_df.iloc[first_third:second_third], use_container_width=True, height=35*first_third+38)
   with col3:
-    st.dataframe(irrVerbs_df.iloc[second_third:], use_container_width=True, height=35*first_third+38)
-
-# st.data_editor(
-#     verbs_df,
-#     column_config={
-#         'Link': st.column_config.LinkColumn(
-#             'Bøyning', display_text='clicky'
-#         ),
-#     },
-#     hide_index=True,
-# )
-#st.dataframe(verbs_df.style.format({'Links': lambda x: f'<a href="{x}">Link</a>'}), escape_html=False)
+    st.dataframe(irr_verbs_df.iloc[second_third:], use_container_width=True, height=35*first_third+38)
